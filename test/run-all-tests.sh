76ceedf92343c6398bda1794af4a1c35e6687e25
justrunme <52860508+justrunme@users.noreply.github.com>
2025-03-21 22:42:49 +0100

    Update run-all-tests.sh

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "🔁 Running all tests..."
 
+# Перейти в папку test
 cd "$(dirname "$0")"
 
+# Пройтись по всем test_*.sh
 for test in test_*.sh; do
   echo "▶️  Running $test"
   bash "$test"
